5e17caa361183b50a123cb23b4e09d7a7b1a5905
tributetothemoon <kkm97351@gmail.com>
2021-01-03 03:16:55 +0900

    [feat]: 타임스탬프 생성 API 토큰과 이미지 파라미터를 받는 로직 구현
    - 토큰을 위한 미들웨어 authUtil.js를 이용해 먼저 토큰을 decode합니다. (routes/timeStamp/index.js 참조 요망)
    - 미들웨어로 사용되어지는 multer를 이용해 이미지 파일을 AWS S3에 업로드하고 해당 url을 timeStampController.createTimeStamp에서 이어받습니다.
    
    related to #9

--- controller/timeStampController.js
+++ controller/timeStampController.js
@@ -3,5 +3,24 @@ const responseMessage = require('../modules/responseMessage');
 const statusCode = require('../modules/statusCode');
 
 module.exports = {
-  createTimeStamp: async (req, res) => {},
+  createTimeStamp: async (req, res) => {
+    try {
+      const userId = req.decoded.id;
+      const timeStampImageUrl = req.file.location;
+
+      return res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, '타임스탬프 이미지 S3 업로드 완료', timeStampImageUrl));
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          util.fail(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR,
+          ),
+        );
+    }
+  },
 };
--- routes/timeStamp/index.js
+++ routes/timeStamp/index.js
@@ -4,8 +4,10 @@ const authUtil = require('../../middlewares/authUtil');
 
 const timeStampController = require('../../controller/timeStampController');
 
+const upload = require('../../modules/multer');
+
 const router = express.Router();
 
-router.post('/', authUtil.checkToken, timeStampController.createTimeStamp); // create timeStamp controller function required
+router.post('/', authUtil.checkToken, upload.single('image'), timeStampController.createTimeStamp); // create timeStamp controller function required
 
 module.exports = router;
