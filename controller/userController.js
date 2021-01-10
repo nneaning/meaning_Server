04e9b7f0d49be0e7f0aaf5f7152568914c22c52c
tributetothemoon <kkm97351@gmail.com>
2021-01-10 18:17:46 +0900

    [feat]: 오늘 하루 다짐 작성 API, 날짜 정보를 추가로 받음
    - 어느 날짜에 보여줄지를 'YYYY-MM-DD' 포맷으로 받습니다.
    - 날짜 데이터에 대한 검증 로직이 추가되었습니다.
    
    related to #46

--- controller/userController.js
+++ controller/userController.js
@@ -185,13 +185,20 @@ module.exports = {
   },
   createDailyMaxim: async (req, res) => {
     try {
-      const { todaysPromiseContents } = req.body;
+      const { todaysPromiseContents, date } = req.body;
 
       if (!todaysPromiseContents) {
         res
           .status(statusCode.BAD_REQUEST)
           .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
       }
+
+      if (!dateTimeModule.checkValidDateFormat(date)) {
+        res
+          .status(statusCode.BAD_REQUEST)
+          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.INVALID_DATE_FORMAT));
+      }
+
       const checkDailyMaxim = await userService.checkDailyMaxim(todaysPromiseContents);
 
       if (checkDailyMaxim) {
--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -14,7 +14,6 @@ module.exports = {
   /* 온보드 정보 등록 */
   UPDATE_ONBOARD_SUCCESS: '온보드 정보 등록 성공',
   UPDATE_ONBOARD_FAIL: '온보드 정보 등록 실패',
-  INVALID_TIME_FORMAT: '유효하지 않은 시간 정보 포맷입니다.',
 
   /* 회원관리 */
   ALREADY_ID: '존재하는 ID 입니다.',
@@ -28,7 +27,11 @@ module.exports = {
   CREATE_TIMESTAMP_FAIL: '타임스탬프 생성 실패',
   READ_TIMESTAMP_ALL_SUCCESS: '전체 타임스탬프 조회 성공',
   READ_TIMESTAMP_ALL_FAIL: '전체 타임스탬프 조회 실패',
+
+  /* 시간, 날짜 포맷 */
   INVALID_DATETIME_FORMAT: '유효하지 않은 날짜, 시간 정보 포맷입니다',
+  INVALID_TIME_FORMAT: '유효하지 않은 시간 정보 포맷입니다.',
+  INVALID_DATE_FORMAT: '유효하지 않은 날짜 정보 포맷입니다.',
 
   /* 오늘 하루 다짐 */
   CREATE_DAILYMAXIM_SUCCESS: '오늘 하루 다짐 생성 완료',
