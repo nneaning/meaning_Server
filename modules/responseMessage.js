c9ffdbd3a28594d73972739b00b11b260c33fea4
tributetothemoon <kkm97351@gmail.com>
2021-01-14 02:52:25 +0900

    [fix]: 그룹 생성 시 그룹명 중복 검사 및 응답 코드 구분화
    - 그룹 생성 시 DB에서 이미 해당 그룹명을 가지고 있는지 검사하는 로직을 추가했습니다.
    - 또한 요청 거부시 내부오류를 제외하고 모두 Status Code 400으로 응답하는 것을 수정했습니다.
    
    related to #31

--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -73,6 +73,7 @@ module.exports = {
   CREATE_GROUP_FAIL: '그룹 생성 실패',
   NO_GROUP: '가입된 그룹이 없습니다.',
   ALREADY_GROUP: '이미 그룹에 속해 있습니다.',
+  ALREADY_GROUP_NAME: '이미 있는 그룹명입니다.',
   JOIN_GROUP_SUCCESS: '그룹 참가 완료',
   JOIN_GROUP_FAIL: '그룹 참가 실패',
   READ_GROUP_SUCCESS: '그룹 조회 성공',
